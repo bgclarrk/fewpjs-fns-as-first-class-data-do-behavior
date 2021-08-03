0cf8d74812e63dc05d8f93de919ba4b830451ab8
+ <+>
2021-08-03 10:55:33 -0700

    Done.

--- index.js
+++ index.js
@@ -11,3 +11,21 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+
+function greet(time) {
+  const timeSplit = time.split(':');
+  const hour = parseInt(timeSplit[0]);
+
+  if (hour < 12) {
+    return "Good Morning";
+  } else if (hour > 12 && hour < 17) {
+    return "Good Afternoon";
+  } else {
+    return "Good Evening";
+  }
+};
+
+function displayMessage(message) {
+  const greeting = document.querySelector("#greeting");
+  greeting.innerHTML = message;
+}
